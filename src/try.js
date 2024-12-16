4384f178634b19f2e2ffc200fde0afa5d037891f
joh66888 <joh66888@gmail.com>
2024-12-16 13:44:33 +0800

    Switch to existing universe.hdr file

--- src/try.js
+++ src/try.js
@@ -42,7 +42,7 @@ pmremGenerator.compileEquirectangularShader();
 
 // Load the HDR environment map
 rgbeLoader.load(
-    '/textures/environmentMap/universe1.hdr',
+    '/textures/environmentMap/universe.hdr',
     function(texture) {
         console.log('HDR texture loaded, processing...');
         texture.mapping = THREE.EquirectangularReflectionMapping;
